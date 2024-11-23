c0982cc57076439d44aa105e0b44d8a3b30ca48b
Alex Pochishchalov <pirojekhell@mail.ru>
2024-11-23 22:43:21 +0300

    Добавлены файлы примеров. Заполнен Readme файл.

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,12 +1,13 @@
 
 #include <iostream>
+#include <fstream>
 
 #include "json_reader.h"
 
 using namespace std;
 
 int main() {
-    
+
     transport_catalogue::TransportCatalogue catalogue;
 
     reader::JsonReader reader(cin);
@@ -19,7 +20,26 @@ int main() {
 
     handler::RequestHandler handler(catalogue, renderer, route);
 
-    const auto doc = reader.GetInfo(handler);
+    const auto doc_json = reader.GetInfo(handler);
 
-    json::Print(doc, cout);
+    svg::Document doc_svg = handler.RenderMap();
+
+    ofstream out_json("out.json"s);
+
+    if (!out_json) {
+        cerr << "невозможно открыть файл 'out.json' для вывода"s << endl;
+    }
+    else {
+        json::Print(doc_json, out_json);
+    }
+
+    ofstream out_svg("out_image.svg"s);
+
+    if (!out_svg) {
+        cerr << "невозможно открыть файл 'out.json' для вывода"s << endl;
+    }
+    else {
+        doc_svg.Render(out_svg);
+    }
+
 }
